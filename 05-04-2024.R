077a272bf9fbbfaa76012a222aafa895a957ff7d
harsharajchalla <160366531+harsharajchalla@users.noreply.github.com>
2024-05-04 23:19:33 -0400

    Add files via upload

---
+++ 05-04-2024.R
@@ -0,0 +1,34 @@
+# Define the numbers
+num1 <- 7
+num2 <- 12
+num3 <- 5
+
+# Calculate the average
+average <- (num1 + num2 + num3) / 3
+
+# Print the average
+print(paste("The average of the numbers is:", average))
+
+
+# Print the welcome message
+first_name <- "Harsharaj Challa"
+print(paste("Welcome to R,", first_name))
+
+
+# Define the variables
+x <- 7
+y <- 12
+z <- 4
+
+# Check if x is bigger than y and z
+if (x > y) {
+  print("x is bigger than y")
+} else {
+  print("x is not bigger than y")
+}
+
+if (x > z) {
+  print("x is bigger than z")
+} else {
+  print("x is not bigger than z")
+}
